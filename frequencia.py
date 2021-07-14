3c9e1a3e6a96dac94fe4f3e3ea17993c3ce12f59
Flavius Gorgonio <flgorgonio@gmail.com>
2021-07-14 00:13:58 -0300

    Formatting by PEP8

--- frequencia.py
+++ frequencia.py
@@ -1,5 +1,6 @@
 import PyPDF2
 
+
 pdfFileObj = open("listapresenca_DCT1101_2021.1_01.pdf", "rb")
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 numPages = pdfReader.numPages
@@ -8,8 +9,6 @@ print("Páginas:",numPages)
 
 pageObj = pdfReader.getPage(0)
 text = pageObj.extractText()
-
-
 
 start = text.find("ASSINATURA") + len("ASSINATURA")
 print("Posição:", start)
@@ -44,11 +43,13 @@ for letter in text:
 
 	previous = letter
 wordList += [word]
+
 print()
 print(wordList)
 
 students = {}
 numStudents = len(wordList) // 2
+
 for i in range(numStudents):
 	name = wordList[2*i]
 	matr = wordList[2*i+1]
@@ -59,6 +60,8 @@ for i in range(numStudents):
 	students[matr] = name
 
 print()
+
 for stud in students:
 	print(stud, students[stud])
+
 print()
